f349443d01a54da8d7e2d6c2185e54057deea5f4
rupalirajesh <152160954+rupalirajesh@users.noreply.github.com>
2026-04-20 13:07:44 +0530

    Update basic_functions.py

--- src/basic_functions.py
+++ src/basic_functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from transformers import pipeline
 from groq import Groq
-from langchain_ollama import OllamaLLM
+# from langchain_ollama import OllamaLLM
 import json
 import re
 
